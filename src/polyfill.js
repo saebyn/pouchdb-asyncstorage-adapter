a5dbd071681ba0b304a84f79bded7fa558c71173
saebyn <saebynx+github@gmail.com>
2025-06-26 18:49:05 -0700

    Update polyfill.js

--- src/polyfill.js
+++ src/polyfill.js
@@ -7,4 +7,3 @@ global.btoa = global.btoa || require('btoa')
 require('blob-polyfill')
 
 if (!process.version) process.version = 'core-js'
-process.nextTick = process.nextTick || global.setImmediate
